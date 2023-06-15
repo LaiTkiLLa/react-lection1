69d73139f5b84ab2374c593c493fd7d72fa88616
User Solber <v.goncharov@solber.ru>
2023-06-15 21:01:04 +0300

    povtorno

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -1,9 +1,11 @@
 import './main.css';
 import {Calendar} from "./components/calendar";
 
+const now = new Date(2023, 5, 15);
+
 function App() {
   return (
-      <Calendar />
+      <Calendar now={now}/>
   );
 }
 
--- calendar/src/components/calendar.jsx
+++ calendar/src/components/calendar.jsx
@@ -1,10 +1,13 @@
+import {CalendarDatesComponent} from "./calendar-dates.component";
 
-const now = new Date(2017, 2, 8);
 const dayName = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
 const monthName = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря']
 const month = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
 
-export const Calendar = () => {
+export const Calendar = ({now}) => {
+    const allDayOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate()
+    const allDaysOfPrevMonth = new Date(2023, now.getMonth(),0 ).getDate()
+    const allDaysOfNextMonth = new Date(2023, now.getMonth() + 2,0).getDate()
     return (
         <div className="ui-datepicker">
             <div className="ui-datepicker-material-header">
@@ -42,53 +45,11 @@ export const Calendar = () => {
                     <th scope="col" title="Воскресенье">Вс</th>
                 </tr>
                 </thead>
-                <tbody>
-                <tr>
-                    <td className="ui-datepicker-other-month">27</td>
-                    <td className="ui-datepicker-other-month">28</td>
-                    <td>1</td>
-                    <td>2</td>
-                    <td>3</td>
-                    <td>4</td>
-                    <td>5</td>
-                </tr>
-                <tr>
-                    <td>6</td>
-                    <td>7</td>
-                    <td className="ui-datepicker-today">8</td>
-                    <td>9</td>
-                    <td>10</td>
-                    <td>11</td>
-                    <td>12</td>
-                </tr>
-                <tr>
-                    <td>13</td>
-                    <td>14</td>
-                    <td>15</td>
-                    <td>16</td>
-                    <td>17</td>
-                    <td>18</td>
-                    <td>19</td>
-                </tr>
-                <tr>
-                    <td>20</td>
-                    <td>21</td>
-                    <td>22</td>
-                    <td>23</td>
-                    <td>24</td>
-                    <td>25</td>
-                    <td>26</td>
-                </tr>
-                <tr>
-                    <td>27</td>
-                    <td>28</td>
-                    <td>29</td>
-                    <td>30</td>
-                    <td>31</td>
-                    <td className="ui-datepicker-other-month">1</td>
-                    <td className="ui-datepicker-other-month">2</td>
-                </tr>
-                </tbody>
+                <CalendarDatesComponent allDayOfMonth={allDayOfMonth}
+                                        allDaysOfPrevMonth={allDaysOfPrevMonth}
+                                        allDaysOfNextMonth={allDaysOfNextMonth}
+                                        now={now}
+                                        dayName={dayName}/>
             </table>
         </div>
     )
